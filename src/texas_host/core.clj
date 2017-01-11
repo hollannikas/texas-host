1ec1692a6ff25006d0829305c4a538225d043336
hollannikas <hollannikas@users.noreply.github.com>
2017-01-11 12:43:19 +0200

    Pick a card, any card

--- src/texas_host/core.clj
+++ src/texas_host/core.clj
@@ -1,7 +1,29 @@
 (ns texas-host.core
   (:gen-class))
 
+(defn remove-card [card pack]
+  (remove
+    (fn [x] (= x card))
+    pack))
+
+(defn get-card [pack]
+  ( let [card (rand-nth pack)]
+  [card (remove-card card pack)]))
+
+;; should return set of cards (size= amount) and a pack without those cards
+(defn get-cards [amount pack]
+  (get-card pack))
+
+
+(defn deal-cards [amount pack]
+  (get-cards amount pack))
+
+(defn deal-hand [player]
+  (let [pack (into [] (range 52))]
+  (deal-cards 2 pack)))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (str "hello "(deal-hand "john"))))
